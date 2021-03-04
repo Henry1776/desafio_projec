4d0bbb58933fc24451553b182665bb656cacb7be
henry <henryartavia767@hotmail.com>
2021-03-04 17:13:21 -0600

    finish project up heroku

---
+++ app/models/project.rb
@@ -0,0 +1,8 @@
+class Project < ApplicationRecord
+  validates :name, presence:true
+  validates :description, presence:true
+  validates :status, presence:true
+
+
+  enum status: [:proposal, :progress, :finished]
+end
